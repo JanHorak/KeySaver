0c68392c56b1591a5da369e111280916792ea7b8
JanHorak <janhorak@gmx.de>
2013-09-05 08:56:21 +0200

    UnitTest extended, Version 0.5Beta Released

--- KeySaver/test/net/jan/test/XMLtests/TestXMLFunctions.java
+++ KeySaver/test/net/jan/test/XMLtests/TestXMLFunctions.java
@@ -5,12 +5,11 @@ package net.jan.test.XMLtests;
  * and open the template in the editor.
  */
 
+import java.io.File;
 import net.jan.keysaver.manager.FileManager;
+import net.jan.keysaver.sources.Category;
 import net.jan.keysaver.sources.CategoryList;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import net.jan.keysaver.sources.Key;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -22,6 +21,26 @@ public class TestXMLFunctions {
     
     public TestXMLFunctions() {
     }
+    /**
+     * This Tests need the Default-Structure of the XML-File
+     * Warning: Run of the Test will reset the Data of the File!
+     */
+    
+    @Test
+    public void shouldCheckAvailibilityOfStructure(){
+        FileManager fm = new FileManager();
+        fm.checkAvailibility();
+        
+        //Points the File for Deleting
+        File f = new File("AppData\\structure.xml");
+        
+        //Delete the File
+        f.delete();
+        
+        //Detection and restore
+        fm.checkAvailibility();
+    }
+    
     
     @Test
     public void shouldLoadCategoryListFromXML(){
@@ -29,7 +48,25 @@ public class TestXMLFunctions {
         catList = new FileManager().returnListofCategories();
         
         assertNotNull(catList);
-        
         catList.printStructure();
+    
     }
+    
+    @Test
+    public void shouldReturnSingleCategoryListFromXML(){
+        Category cat = new Category();
+        cat = new FileManager().returnSingleCategory("default");
+        System.out.println(cat.getName());
+        System.out.println(cat.getIconPath());
+        assertNotNull(cat);
+    }
+    
+    @Test
+    public void shouldReturnKeyFromXML(){
+        Key k = new Key();
+        k = new FileManager().returnKey("defaultKey");
+        assertNotNull(k);
+    }
+    
+    
 }
